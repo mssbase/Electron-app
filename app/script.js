036737c62ed256b360aa40bd526298e9a3f9ed00
Michal S <michalsienczuk@gmail.com>
2021-04-26 18:21:07 +0200

    add stoptimer

--- app/script.js
+++ app/script.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 
 class App extends React.Component {
     state = {
-        status: "Off",
+        status: 'off',
         time: 0,
         timer: null,
     }
@@ -12,27 +12,64 @@ class App extends React.Component {
         let seconds = time%60;
         let minutes = Math.floor(time/60);
         if(seconds < 10)seconds = '0' + seconds;
-        if(minutes > 10)minutes = '0' + minutes;
+        if(minutes < 10)minutes = '0' + minutes;
         return minutes + ':' + seconds;
-
     }
 
+    step = () => {
+        this.setState({
+            time: this.state.time - 1,
+        });
+
+        if (this.state.time === 0 ) {
+
+            if (this.state.status === 'work') {
+                this.setState({
+                    status: 'rest',
+                    time: 20
+                });
+            } else {
+                this.setState({
+                    status: 'work',
+                    time: 1200
+                });
+            }
+        }
+        return this.state.time;
+    };
+
+    startTimer = () => {
+
+        this.setState({
+            status: 'work',
+            time: 1200,
+            timer: setInterval(this.step, 1200),
+        });
+    };
+
+    stopTimer = () => {
+        this.setState({
+            status: 'off',
+            time: 0,
+            timer: clearInterval(this.state.timer),
+        });
+    };
 
 
     render() {
-        const { status } = this.state;
+        const { status, time } = this.state;
         return (
             <div>
-                {(this.state.status === 'off') && <div>
+                {(status === 'off') && <div>
                     <h1>Protect your eyes</h1>
                     <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
                     <p>This app will help you track your time and inform you when it's time to rest.</p>
                 </div>}
-                {(status === 'work') && <img src="./images/Work.png" />}
-                {(status === 'rest') && <img src="./images/Rest.png" />}
-                {(status !== 'off') && <div className="timer">{this.formatTime(this.state.time)}</div>}
-                {(status === 'off') && <button className="btn">Start</button>}
-                {(status !== 'off') && <button className="btn">Stop</button>}
+                {(status === 'work') && <img src="./images/work.png"/>}
+                {(status === 'rest') && <img src="./images/rest.png"/>}
+                {(status !== 'off') && <div className="timer">{this.formatTime(time)}</div>}
+                {(status === 'off') && <button className="btn" onClick={this.startTimer}>Start</button>}
+                {(status !== 'off') && <button className="btn" onClick={this.stopTimer}>Stop</button>}
                 <button className="btn btn-close">X</button>
             </div>
         )
