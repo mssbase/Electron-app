a891bef46fd2f4be3368e3b86f8c5a25d88ce64d
Michal S <michalsienczuk@gmail.com>
2021-04-26 17:12:23 +0200

    showing items

--- app/script.js
+++ app/script.js
@@ -9,23 +9,24 @@ class App extends React.Component {
     }
 
 
-  render() {
-    return (
-      <div>
-        <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/Work.png"  alt={}/>
-        <img src="./images/Rest.png" alt={}/>
-        <div className="timer">
-          18:23
-        </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
-        <button className="btn btn-close">X</button>
-      </div>
-    )
-  }
+    render() {
+        const { status } = this.state;
+        return (
+            <div>
+                {(this.state.status === 'off') && <div>
+                    <h1>Protect your eyes</h1>
+                    <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+                    <p>This app will help you track your time and inform you when it's time to rest.</p>
+                </div>}
+                {(status === 'work') && <img src="./images/Work.png" />}
+                {(status === 'rest') && <img src="./images/Rest.png" />}
+                {(status !== 'off') && <div className="timer">18:23</div>}
+                {(status === 'off') && <button className="btn">Start</button>}
+                {(status !== 'off') && <button className="btn">Stop</button>}
+                <button className="btn btn-close">X</button>
+            </div>
+        )
+    };
 }
 
 render(<App />, document.querySelector('#app'));
